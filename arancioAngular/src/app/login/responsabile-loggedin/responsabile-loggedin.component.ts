13279aa80ad15e4e809429bb6fa8ee4294dd41ad
danie <daniele.pulvirenti@outlook.com>
2022-02-11 11:10:44 +0100

    FrontEnd: bug fix

--- arancioAngular/src/app/login/responsabile-loggedin/responsabile-loggedin.component.ts
+++ arancioAngular/src/app/login/responsabile-loggedin/responsabile-loggedin.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Dipendente } from 'src/app/models/dipendente';
+import { dipendenteLoggato } from 'src/app/models/dipendenteLoggato';
+import { LoginService } from 'src/app/services/login.service';
+import { DipendenteLoginComponent } from '../dipendente-login/dipendente-login.component';
 
 @Component({
   selector: 'app-responsabile-loggedin',
@@ -7,9 +12,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResponsabileLoggedinComponent implements OnInit {
 
-  constructor() { }
+  model : dipendenteLoggato = new dipendenteLoggato();
+  user : any = sessionStorage.getItem("utente")|| '{}';
+  utente : Dipendente= new Dipendente();
+  constructor(private router : Router, private login : DipendenteLoginComponent, private authService : LoginService) { }
+
+  getStorage(){
+    this.utente = JSON.parse(sessionStorage.getItem("utente")|| '{}');
+  }
+
+  elencoDipendenti(){
+    this.router.navigate(["/allDip"])
+  }
+  
+  elencoDipendentiConContratto(){
+    this.router.navigate(["/allDipContracts"])
+  }
 
   ngOnInit(): void {
+    if(!this.authService.isLoggedIn$){
+      this.router.navigate(["/dipLogin"])
+    }
+    this.utenteLoggedIn(this.login.utenteLoggato)
+    this.getStorage()
   }
+
+
+ utenteLoggedIn(utenteLoggato : any){
+   return this.model.email = utenteLoggato;
+
+ }
+
+aggiungiMese(){
+  this.router.navigate(["/addMese"])
+}
+
+elencoContratti(){
+  this.router.navigate(["/allContratti"])
+}
+
+cambiaPassword(){
+  this.router.navigate(["/changePassword"])
+}
+
 
 }
--- arancioAngular/src/app/services/login.service.ts
+++ arancioAngular/src/app/services/login.service.ts
@@ -1,12 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { Dipendente } from '../models/dipendente';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
   public isLoggedIn$: BehaviorSubject<boolean>;
-  constructor() {
+  private backendURL :String = "http://localhost:8080/arancioRest/gestione_presenze/dipendente";
+  constructor(private http : HttpClient) {
     const isLoggedIn = localStorage.getItem('loggedIn') === 'true';
     this.isLoggedIn$ = new BehaviorSubject(isLoggedIn);
   }
@@ -24,4 +27,28 @@ export class LoginService {
     this.isLoggedIn$.next(false);
   }
 
+  public loginAuth(model: Dipendente,onSuccess:any,onFailure:any) : void {
+    this.doPost("/login",model,onSuccess,onFailure);
+  }
+
+  private doPost(querystring:any, data:any, onSuccess:any,onFailure:any){
+    
+    var url = this.backendURL + "" + querystring;
+
+
+
+     return this.http.post(url,data).subscribe((httpResponse:any) => {
+               console.log(httpResponse);
+               console.log(data);
+           
+     if(httpResponse.successo == true){
+       onSuccess(httpResponse.data);
+     } else {
+       onFailure(httpResponse.codice_errore);
+       alert("Operazione non andata a buon fine. Codice errore: "+httpResponse.codice_errore);
+       
+     }       
+                 
+     });
+}
 }
