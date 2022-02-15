14442db15ac7cd8aa5b79633a5bcabe409c4d3a4
DamianoFuselli <damiano.fuselli@gmail.com>
2022-02-15 16:33:41 +0100

    aggiunto inserimento automatico delle presenza all'apertura di un nuovo mese

--- arancioEJB/ejbModule/it/exolab/dao/MeseDAO.java
+++ arancioEJB/ejbModule/it/exolab/dao/MeseDAO.java
@@ -1,6 +1,9 @@
 package it.exolab.dao;
 
 import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
@@ -11,18 +14,19 @@ import it.exolab.mapper.MeseMapper;
 import it.exolab.model.Dipendente;
 import it.exolab.model.DipendenteMese;
 import it.exolab.model.Mese;
+import it.exolab.model.Presenza;
 import it.exolab.util.MyBatisUtils;
 
-public class MeseDAO extends BaseDAO<MeseMapper>{
-	
-	private static MeseDAO istanza=null;
-	
+public class MeseDAO extends BaseDAO<MeseMapper> {
+
+	private static MeseDAO istanza = null;
+
 	private MeseDAO() {
 	}
-	
+
 	public static MeseDAO getIstanza() {
-		if(istanza==null) {
-			istanza=new MeseDAO();
+		if (istanza == null) {
+			istanza = new MeseDAO();
 		}
 		return istanza;
 	}
@@ -34,19 +38,43 @@ public class MeseDAO extends BaseDAO<MeseMapper>{
 		mapper.insert(mese);
 		sqlSession.commit();
 		inserisciDipendeti_Mesi();
+		inserisciPresenzeDelMese(mese.getMese());
 	}
-	
-	public void inserisciDipendeti_Mesi() throws ErroreGenerico {
+
+	private void inserisciDipendeti_Mesi() throws ErroreGenerico {
 		SqlSession sqlSession = MyBatisUtils.getSqlSessionFactory().openSession();
 		MeseMapper mapper = sqlSession.getMapper(MeseMapper.class);
 		Integer id_mese = mapper.selectLastID();
-		List<Dipendente> id_dipendenti=mapper.selectAllID();
+		List<Dipendente> id_dipendenti = mapper.selectAllID();
 		DipendenteMese dipMes = new DipendenteMese();
 		dipMes.setId_mese_fk(id_mese);
 		for (Dipendente id_dipendente : id_dipendenti) {
 			dipMes.setId_dipendente_fk(id_dipendente.getId_dipendente());
 			mapper.inserisciDipendenti_Mesi(dipMes);
 			sqlSession.commit();
+		}
+	}
+
+	private void inserisciPresenzeDelMese(Date data) throws ErroreGenerico {
+		SqlSession sqlSession = MyBatisUtils.getSqlSessionFactory().openSession();
+		MeseMapper mapper = sqlSession.getMapper(MeseMapper.class);
+		List<Dipendente> id_dipendenti = mapper.selectAllID();
+		Presenza presMes = new Presenza();
+		presMes.setId_mese_fk(mapper.selectLastID());
+		SimpleDateFormat giorno = new SimpleDateFormat("dd");
+		SimpleDateFormat mese = new SimpleDateFormat("MM");
+		SimpleDateFormat anno = new SimpleDateFormat("yyyy");
+		LocalDate localDate = LocalDate.of(Integer.parseInt(anno.format(data)), Integer.parseInt(mese.format(data)), Integer.parseInt(giorno.format(data)));
+		Calendar cal = Calendar.getInstance();
+		Integer res = cal.getActualMaximum(Calendar.DATE);
+		for (Dipendente id_dipendente : id_dipendenti) {
+			presMes.setId_dipendente_fk(id_dipendente.getId_dipendente());
+			for (Integer i = localDate.getDayOfMonth(); i <= res; i++) {
+				presMes.setData(Date.valueOf(localDate));
+				mapper.inserisciPresenzeMese(presMes);
+				sqlSession.commit();
+				localDate = localDate.plusDays(1);
+			}
 		}
 	}
 
--- arancioEJB/ejbModule/it/exolab/mapper/MeseMapper.java
+++ arancioEJB/ejbModule/it/exolab/mapper/MeseMapper.java
@@ -6,11 +6,13 @@ import java.util.List;
 import it.exolab.model.Dipendente;
 import it.exolab.model.DipendenteMese;
 import it.exolab.model.Mese;
+import it.exolab.model.Presenza;
 
 public interface MeseMapper {
 	
 	void insert(Mese mese);
 	void inserisciDipendenti_Mesi(DipendenteMese dipMes);
+	void inserisciPresenzeMese(Presenza presenza);
 	void update(Mese mese);
 	void delete(Integer id_mese);
 	Mese selectByMese(Date mese);
